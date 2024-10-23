29e067f7abbfe4cf0309444a326a6eb25515bbb3
Sikandar Chauhan <sikandarc989@gmail.com>
2024-10-23 21:00:31 +0530

    merge fix

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,25 +1,4 @@
 "use client"
-<<<<<<< HEAD
-import { useSession, signIn, signOut } from "next-auth/react"
-
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
-  )
-}
-=======
 import React from 'react'
 
 const page = () => {
@@ -29,4 +8,3 @@ const page = () => {
 }
 
 export default page
->>>>>>> f8426a92c2ab48cf9ad1ac466f9c367d09268626
